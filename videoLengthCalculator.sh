0401cec29e9afddf243ecf281dd51c4f4a8c6b7e
m00nbek <melikulov990@gmail.com>
2022-09-15 11:33:52 +0500

    rewrote the whole goddamn script

--- videoLengthCalculator.sh
+++ videoLengthCalculator.sh
@@ -1,17 +1,31 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-LENGTH=0
+LENGTH=00:00:00
 
 echo Calculating...
-LOOP=0
 
-for video in $(ls | grep .mp4)
-do
-	LOOP=$(($LOOP+1))
-	TIME=$(ffmpeg -i $video 2>&1 | grep Duration | cut -d ' ' -f 4 | sed s/,//)
-	TIME=${TIME::8}
-	LENGTH=$(( $LENGTH + $(echo $TIME | awk -F: '{ print ($1 * 3600) + ($2 * 60) + $3 }')))
+# Get all the video duration & save them to durations.txt
+for video in *.mp4; do
+  echo $(ffmpeg -i "$video" 2>&1 | grep Duration | cut -d ' ' -f 4 | sed s/,//) >> durations.txt
 done
 
-echo LOOP: $LOOP
-echo RESULT: $(date -u -r $LENGTH +%T)
+
+# Read the durations.txt and calculate all the durations
+echo $(awk -F: '{ n=0; for(i=NF; i>=1; --i) secs += $i * 60 ^ n++ } END { print secs }' durations.txt) >> seconds.txt
+
+# Turn the seconds into date format
+LENGTH=$(awk -F: '
+  { n=0; for(i=NF; i>=1; --i) secs += $i * 60 ^ n++ }
+  END {
+    hours   = int(secs / 3600)
+    minutes = int((secs - hours * 3600) / 60)
+    secs    = secs % 60
+    printf "%02d:%02d:%05.2f\n", hours, minutes, secs
+  }
+  ' seconds.txt)
+
+# clean up
+rm -rf durations.txt seconds.txt
+
+#final result
+echo RESULT: $LENGTH
